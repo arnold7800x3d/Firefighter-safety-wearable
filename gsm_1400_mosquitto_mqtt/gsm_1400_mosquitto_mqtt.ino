efc90c12ae32862e612c408c9e53d27f464e73a8
arnold7800x3d <arnold.7800x3d@gmail.com>
2026-02-17 00:32:45 +0300

    Added dht functionality

--- gsm_1400_mosquitto_mqtt/gsm_1400_mosquitto_mqtt.ino
+++ gsm_1400_mosquitto_mqtt/gsm_1400_mosquitto_mqtt.ino
@@ -1,9 +1,40 @@
-void setup() {
-  // put your setup code here, to run once:
+/* 
+  firmware for the firefighter safety wearable.
+*/
 
+// libraries
+#include <DHT.h>
+#include "secrets.h"
+
+// symbolic constants
+#define DHTPIN 6
+#define DHTTYPE DHT22
+
+// objects
+DHT dht(DHTPIN, DHTTYPE);
+
+// variables
+float humidity;
+float temperature;
+
+void setup() {
+  Serial.begin(9600);
+  dht.begin();
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+  humidity = dht.readHumidity();
+  temperature = dht.readTemperature();
 
+  if (isnan(humidity) || isnan(temperature)){
+    Serial.println("Failed to read from DHT sensor!");
+  } else {
+    Serial.print("Humidity: ");
+    Serial.print(humidity);
+    Serial.print(" %, Temperature: ");
+    Serial.print(temperature);
+    Serial.println(" °C");
+  }
+
+  delay(1000);
 }
